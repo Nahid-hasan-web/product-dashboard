adc391116cefcd62d84887f27083c644112baa38
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-09-16 17:28:32 +0600

    adfas

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,14 +1,26 @@
-const uploadImage = require("../helpers/uploadImage")
+const authModel = require('../models/authModel');
 
-const addCatagory = (req ,res)=>{
+const cloudinary = require('cloudinary').v2
+
+    cloudinary.config({ 
+        cloud_name: 'dorn2tiyl', 
+        api_key: '325214164479862', 
+        api_secret: 'pp5uWG8ynmAPXSQqW7lYsaewAZE' 
+    });
+    
+
+const addCatagory = async (req ,res)=>{
     // const {catagoryImage} = req.body
     
-    res.send(req.file)
-    uploadImage(req.file.path)
-    .then((res)=>console.log(res , 'ok'))
-    .catch((err)=>console.log(err))
+    // const productImage = await cloudinary.uploader.upload(req.file.path, {public_id:Date.now() ,})
 
 
+    const createdUser = await authModel.find({email:req.user.email})       
+
+
+
+    res.send(createdUser)
+    
     
 
 }
