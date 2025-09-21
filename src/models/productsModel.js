c8abb63d690dd5bd17417546383314c81fbce0df
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-09-21 23:22:45 +0600

    product schema created

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,22 +1,69 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  address: [
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+  stock: {
+    type: String,
+    required: String,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  subImages: [],
+  price: {
+    type: String,
+    requried: true,
+  },
+  discontPrice: {
+    type: String,
+    default: null,
+  },
+  discountPercent: {
+    type: String,
+    default: null,
+  },
+
+  category: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    default: "pending",
+    enum: ["pending", "active", "reject"],
+  },
+  varients: [
     {
-      rode: {
-        type: String,
-        required: true,
-      },
-      arrea: {
-        type: String,
-        required: true,
-      },
-      city: {
-        type: String,
-        required: true,
-      },
-    }
+      varientName: { type: String, default: null },
+      options: [
+        {
+          color: { type: String, default: null },
+          size: { type: String, default: null },
+          additionalPrict: {
+            type: String,
+            default: null,
+          },
+        },
+      ],
+    },
   ],
+  title: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("products", productSchema);
