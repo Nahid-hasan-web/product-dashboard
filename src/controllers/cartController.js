6b587fd633d3f7ab7df135f97d1bb8155095b60f
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-10-23 19:24:37 +0600

    add to cart done

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,12 +1,38 @@
-const cartModel = require("../models/cartModel")
+const cartModel = require("../models/cartModel");
 
 // --------------------------------------------- Add to cart controller -----------------------------------------
-const addToCart = async (req,res)=>{
-    const {productId , userId} = req.body
+const addToCart = async (req, res) => {
+  try {
+    const { userId, cartItem } = req.body;
 
-    const exisistUser = await  cartModel({userId}) 
+    if (!userId || !cartItem)
+      return req.status(404).send("user id and product id requried");
 
-    res.send(exisistUser)
-}
+    const exisistCart = await cartModel.findOne({ userId });
 
-module.exports = {addToCart}
+    if (exisistCart) {
+      cartItem.map((item) => {
+        const exisitProduct = exisistCart.cartItem.find(
+          (cp) => cp.productId == item.productId
+        );
+        if (exisitProduct) {
+          exisitProduct.qty += 1;
+        } else {
+          exisistCart.cartItem.push(item);
+        }
+      });
+      await exisistCart.save();
+
+      return res.status(200).send("update product cart");
+    }
+
+    await cartModel({ userId, cartItem }).save();
+
+    res.send('cart added sucessfully');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal server error");
+  }
+};
+
+module.exports = { addToCart };
--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -3,13 +3,15 @@ const mongoose = require("mongoose");
 const cartSchema = mongoose.Schema({
   userId: {
     type: mongoose.Schema.ObjectId,
+    ref:'auth',
     required: true,
   },
-  items: [
+  cartItem: [
     {
-      productid: {
+      productId: {
         type: mongoose.Schema.ObjectId,
         required: true,
+        ref:'products'
       },
       qty:{
         type:Number,
