807f4f0f150991fbfa86df7dbbaa813ce1ee6f7a
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-10-31 18:51:18 +0600

    cupon work done nedd to test

--- src/controllers/cuponController.js
+++ src/controllers/cuponController.js
@@ -1,18 +1,54 @@
-const cuponModel = require("../models/cuponModel")
+const cuponModel = require("../models/cuponModel");
 
-const add_cupon = (req,res)=>{
-    const {cuponCode , discountPirce} = req.body
+const add_cupon = (req, res) => {
+  try {
+    const { cuponCode, discountPirce } = req.body;
 
-    if(!cuponCode || !discountPirce) return res.status(404).send('all fildes required')
+    if (!cuponCode || !discountPirce)
+      return res.status(404).send("all fildes required");
+
+    new cuponModel({ cuponCode, discountPirce }).save();
+
+    res.status(200).send("cupon added");
+  } catch (err) {
+    res.send(`Internal server error ${err}`);
+  }
+};
+
+const update_cupon =  async (req, res) => {
+  try {
+    const { cuponId, cuponCode, discountPirce } = req.body;
+
+    const exisistCupon = await cuponModel.find({_id:cuponId})
+
+    if(cuponCode) exisistCupon.cuponCode = cuponCode
+    if(discountPirce) exisistCupon.discountPirce = discountPirce
+
+    exisistCupon.save()
 
 
-    new cuponModel({cuponCode , discountPirce}).save()
+  } catch (err) {
+    res.send(`Internal server error ${err}`);
+  }
+};
+const get_cupon =  async (req, res) => {
+  try {
+    const allCupon = await cuponModel.find()
+    
+    res.status(200).send(allCupon)
+  } catch (err) {
+    res.send(`Internal server error ${err}`);
+  }
+};
+const delete_cupon =  async (req, res) => {
+  try {
+    const {cuponId} = req.body
+   await cuponModel.findByIdAndDelete({cuponId})
+    
+    res.status(200).send('cupon deleted sucessfully')
+  } catch (err) {
+    res.send(`Internal server error ${err}`);
+  }
+};
 
-
-
-
-
-    res.status(200).send('cupon added')
-}
-
-module.exports = {add_cupon}
+module.exports = { add_cupon, update_cupon ,get_cupon,delete_cupon};
--- src/routes/api/cuponApi.js
+++ src/routes/api/cuponApi.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { add_cupon } = require('../../controllers/cuponController')
+const { add_cupon, update_cupon, get_cupon } = require('../../controllers/cuponController')
 
 const cuponApi = express.Router()
 
 cuponApi.post('/add-cupon' , add_cupon)
+cuponApi.post('/update-cupon' , update_cupon)
+cuponApi.get('/get-cupon' , get_cupon)
 
 module.exports = cuponApi
