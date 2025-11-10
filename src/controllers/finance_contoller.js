d6fe924800636923f5e4aa0e06dad4686f67ef15
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-11-10 13:18:02 +0600

    revenue  achived controller added

--- src/controllers/finance_contoller.js
+++ src/controllers/finance_contoller.js
@@ -69,5 +69,69 @@ const salesReport = async (req, res) => {
     res.status(500).send(`Internal server error ${err}`);
   }
 };
+// ---------------------------------------------------- revenue report controller -----------------------------------------------------
+const revenueReport  =  async (req,res)=>{
+try{
+  const target = [
+    {
+      targetMo:10,
+      targetAmount:30000
+    },
+    {
+      targetMo:11,
+      targetAmount:40000
+    },
+    {
+      targetMo:12,
+      targetAmount:50000
+    },
+  ]
 
-module.exports = { salesReport };
+    const revenueReport = await orderModel.aggregate([
+        {$group:{
+            _id:{
+                year:{$year:'$orderDate'},
+                month:{$month:'$orderDate'}
+            },
+            totalSale:{$sum:'$totalAmmount'}
+        }},
+      {  $project:{
+            _id:0,
+            year:"$_id.year",
+            month:"$_id.month",
+            totalSale:'$totalSale'
+        }},
+    ])
+
+    const finalReport = revenueReport.map((item)=>{
+      const findTargertInfo = target.find((t)=>t.targetMo == item.month)
+      console.log(findTargertInfo)
+
+      const finalRation = {...item , targtemonth:findTargertInfo.targetMo , targetAmount:findTargertInfo.targetAmount , aciveRevenue:( (item.totalSale / findTargertInfo.targetAmount)*100 ).toFixed(2)+ '%'  }
+
+
+      return finalRation
+
+
+    })
+
+    console.log(finalReport)
+
+
+
+
+
+
+
+
+    res.send(revenueReport)
+}
+catch(err){
+    res.status(500).send(`Internal server error ${err}`)
+}
+}
+
+
+
+
+module.exports = { salesReport  ,revenueReport};
--- src/routes/api/finance.js
+++ src/routes/api/finance.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { salesReport } = require('../../controllers/finance_contoller')
+const { salesReport, revenueReport } = require('../../controllers/finance_contoller')
 const financeApi = express.Router()
 
 // ------------ finance APIs
 financeApi.get('/sales-report' , salesReport)
+financeApi.get('/revenue-report' , revenueReport)
 
 module.exports = financeApi
