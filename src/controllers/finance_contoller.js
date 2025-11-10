ab516956bcc4f833b2087b268c63f1bb987567f3
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-11-10 10:49:10 +0600

    sort added

--- src/controllers/finance_contoller.js
+++ src/controllers/finance_contoller.js
@@ -1,67 +1,73 @@
-const orderModel = require("../models/orderModel")
-
+const orderModel = require("../models/orderModel");
 
 // ---------------------------------------------------- sales report controller -----------------------------------------------------
-const salesReport = async (req,res)=>{
-    try{    
-        // ------------- time duration for daily sales report 
+const salesReport = async (req, res) => {
+  try {
+    // ------------- time duration for daily sales report
 
-        const startTodaysTime = new Date()
-        startTodaysTime.setHours(0 ,0,0,0)
+    const startTodaysTime = new Date();
+    startTodaysTime.setHours(0, 0, 0, 0);
 
-        const endTodaysTime = new Date()
-        endTodaysTime.setHours(23,59,59,999)
+    const endTodaysTime = new Date();
+    endTodaysTime.setHours(23, 59, 59, 999);
 
+    // -------------- daily sales report filteration
+    const dailyReport = await orderModel.aggregate([
+      { $match: { orderDate: { $gt: startTodaysTime, $lt: endTodaysTime } } },
+      {
+        $group: {
+          _id: 1,
+          totalSale: { $sum: 1 },
+          totalSaleAmount: { $sum: "$totalAmmount" },
+        },
+      },
+    ]);
 
-        // -------------- daily sales report filteration
-        const dailyReport =  await orderModel.aggregate([
-            {$match:{orderDate:{$gt:startTodaysTime , $lt:endTodaysTime}}},
-            {$group:{
-                _id:1,
-                totalSale:{$sum:1},
-                totalSaleAmount:{$sum:'$totalAmmount'}
-            }}
-        ])
+    // ------------- time duration for 7 days sales report
+    const today = new Date();
+    const prevSavenDays = new Date();
 
-        // ------------- time duration for 7 days sales report
-        const  today  = new Date()
-        const prevSavenDays = new Date()
+    prevSavenDays.setDate(today.getDate() - 7);
 
-        prevSavenDays.setDate(today.getDate() - 7)
+    const weeklySalesReport = await orderModel.aggregate([
+      { $match: { orderDate: { $gt: prevSavenDays, $lte: today } } },
+      {
+        $group: {
+          _id: { $dateToString: { format: "%d-%m-%Y", date: "$orderDate" } },
+          totalSale: { $sum: 1 },
+          totalSaleInAmount: { $sum: "$totalAmmount" },
+        },
+      },
+      {$sort:{_id:1}}
+    ]);
 
-        console.log(prevSavenDays)
-    
-        const weeklySalesReport = await orderModel.aggregate([
-            {$match:{orderDate:{$gt:prevSavenDays , $lte:today }}},
-            {$group:{
-                _id:{$dateToString:{format:"%d-%m-%Y" , date:'$orderDate'}},
-                totalSale:{$sum:1},
-                totalSaleInAmount:{$sum:'$totalAmmount'}
-            }},
-        ])
+    // ------------- time duration for 1mo  sales report
 
+    const monthlySaleReport = await orderModel.aggregate([
+      {
+        $group: {
+          _id: {
+            year: { $year: "$orderDate" },
+            month: { $month: "$orderDate" },
+          },
+          totalSale: { $sum: 1 },
+          totalSaleInAmount: { $sum: "$totalAmmount" },
+        },
+      },
+      {$project:{
+        _id:0,
+        year:'$_id.year',
+        month:'$_id.month',
+        totalSale:1,
+        totalSaleOnamount:1
+      }},
+    {$sort:{year:1 , month:1}}
+    ]);
 
+    res.send({dailySale: dailyReport ,weeklySale:weeklySalesReport , monthlySale: monthlySaleReport});
+  } catch (err) {
+    res.status(500).send(`Internal server error ${err}`);
+  }
+};
 
-
-
-
-
-
-
-
-
-
-        res.send(weeklySalesReport)
-        
-        
-    }
-    catch(err){
-        res.status(500).send(`Internal server error ${err}`)
-    }
-
-
-}
-
-
-
-module.exports  = {salesReport}
+module.exports = { salesReport };
