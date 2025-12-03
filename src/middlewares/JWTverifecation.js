569c234b4ea4e584c1c3148260fb66f42b541dce
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-12-03 09:28:13 +0600

    jwt messge added

--- src/middlewares/JWTverifecation.js
+++ src/middlewares/JWTverifecation.js
@@ -4,7 +4,7 @@ const jwtVerification = (req, res, next) => {
   try {
     const token = req.headers.authorization;
     
-    if(!token) return  res.status(404).send('Access token not found')
+    if(!token) return  res.status(404).send('invalid token')
 
     const decoded = jwt.verify(token, process.env.jwt_secret);
 
